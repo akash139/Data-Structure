99de72401ce20c1331b53c3295a6dbb7d8ced07f
akash139 <akashbam@gmail.com>
2021-06-03 00:51:01 +0530

    Queue

--- Data Structures/src/Queue/QueueMain.java
+++ Data Structures/src/Queue/QueueMain.java
@@ -1,36 +1,39 @@
 package Queue;
 
 class Queue {
-	int queue[] = new int[5];
 	int size;
 	int front;
 	int rear;
+	int queue[] = new int[5];
 
 	public void enQueue(int data) {
-		if (isEmpty()) {
+		if (!isFull()) {
 			queue[rear] = data;
 			rear = (rear + 1) % 5;
 			size++;
 		} else {
-			isFull();
+			System.out.println("Queue is full!");
 		}
 	}
 
 	public int deQueue() {
-		int data;
-		data = queue[front];
-		front = (front + 1) % 5;
-		size--;
+		int data = queue[front];
+		if (!isEmpty()) {
+			front = (front + 1) % 5;
+			size--;
+		} else {
+			System.out.println("Queue is empty!!");
+		}
 		return data;
-
 	}
 
 	public void show() {
-		System.out.print("Queue elements: ");
+		System.out.print("Queue elements using for loop: ");
 		for (int i = 0; i < size; i++) {
 			System.out.print(queue[(front + i) % 5] + " ");
 		}
 		System.out.println();
+		System.out.print("Queue elements using for-each: ");
 		for (int z : queue) {
 			System.out.print(z + " ");
 		}
@@ -43,9 +46,10 @@ class Queue {
 
 	public boolean isEmpty() {
 		// OR return size == 0;
-		if (front != rear)
-			return false;
-		return true;
+		// if (front != rear)
+		//	return false;
+		// return true;
+		return getSize() ==5;
 	}
 
 	public boolean isFull() {
@@ -60,22 +64,15 @@ public class QueueMain { // follow FIFO
 
 	public static void main(String[] args) {
 		Queue q = new Queue();
-		System.out.println(q.isEmpty());
+		
 		q.enQueue(5);
 		q.enQueue(2);
 		q.enQueue(7);
 		q.enQueue(3);
-
-		q.deQueue();
-		q.deQueue();
-
 		q.enQueue(9);
 		q.enQueue(1);
-
-		System.out.println("Checking full or not: " + q.isFull());
-
-		System.out.println("Size: " + q.getSize());
+		System.out.println("Checking queue is full or not: "+q.isFull());
+		System.out.println("Size is queue: "+q.getSize());
 		q.show();
-		System.out.println(q.isEmpty());
 	}
 }
